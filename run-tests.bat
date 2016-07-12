a3b3d4b8d268dec1ee51b7dbc25a219bedf7b2ad
wall <wwall@yandex.ru>
2016-07-12 13:57:12 +0300

    test

--- run-tests.bat
+++ run-tests.bat
@@ -4,18 +4,18 @@ call install-racr.bat
 rem compile examples
 set LIB=%CD%\lib
 set TEST=%CD%\tests
-set SQL1C=%CD%\src\sql1C
-if  Exist "%SQL1C%\%RB%" (
-	echo Delete dir %SQL1C%\%RB%
-	RD /Q /S %SQL1C%\%RB%
+set LANG=%CD%\src\lang
+if  Exist "%LIB%\sql1C" (
+	echo Delete dir %LIB%\sql1C
+	RD /Q /S %LIB%\lang
 )
 
-echo Compile %SQL1C% example
+echo Compile %LANG% 
 
 echo Compile exception-api.scm
-%SCHEME% ++path "%RACRBIN%" ++path "%LIB%" --install --collections "%LIB%" "%SQL1C%\lang\exception-api.scm" 
+%SCHEME% ++path "%RACRBIN%" ++path "%LIB%" --install --collections "%LIB%" "%LANG%\exception-api.scm" 
 echo Compile lexer.scm
-%SCHEME% ++path "%RACRBIN%" ++path "%LIB%" --install --collections "%LIB%" "%SQL1C%\lang\lexer.scm" 
+%SCHEME% ++path "%RACRBIN%" ++path "%LIB%" --install --collections "%LIB%" "%LANG%\lexer.scm" 
 
 echo Testing test.lexer.scm
 %SCHEME% ++path "%RACRBIN%" ++path "%LIB%"  "%TEST%\test.lexer.scm" 
